43cd6d163fa578e0aafb3eb62a0623f472d17a4c
Paragraph1148 <actuallyamountain@proton.me>
2025-12-15 08:24:15 +0530

    first commit

---
+++ sum_of_n_nat.py
@@ -0,0 +1,5 @@
+def sum_till_n(n):
+    return (n * (n + 1) // 2)
+    
+n = int(input("Enter a natural number: "))
+print(sum_till_n(n))
---
+++ sum_of_sq_till.py
@@ -0,0 +1,9 @@
+def closest_number(n, m):
+    closest = 0
+    min_difference = float('inf')
+    
+
+
+n = 13
+m = 4
+print(closest_number(n, m))
